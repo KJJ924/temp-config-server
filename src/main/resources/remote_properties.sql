9cf795060c0b69f264ec80676ff2c7f554d35d25
KJJ924 <dkansk924@naver.com>
2022-04-17 02:14:49 +0900

    feat: yml 설정 및 configServer 설정

--- src/main/resources/remote_properties.sql
+++ src/main/resources/remote_properties.sql
@@ -10,3 +10,8 @@ create table remote_properties
     modified_at datetime(6) default now(6),
     primary key (id)
 ) engine=InnoDB;
+
+#
+
+INSERT INTO config_server.remote_properties (id, application, profile, label, prop_key, prop_value, created_at, modified_at) VALUES (1, 'jaejoon', 'local', 'test', 'name', 'jaejoon', '2022-04-17 02:09:44', '2022-04-17 02:09:47');
+INSERT INTO config_server.remote_properties (id, application, profile, label, prop_key, prop_value, created_at, modified_at) VALUES (2, 'jaejoon', 'local', 'test', 'location', 'seoul', '2022-04-17 02:09:44', '2022-04-17 02:09:47');
