351e2524de59d57992ab6d03fa33cf833ba4b5b9
KJJ924 <dkansk924@naver.com>
2022-12-03 23:10:55 +0900

    feat: circuit apply

--- circuit-test-server/src/main/java/me/jaejoon/circuit_test_server/CircuitTestServer.java
+++ circuit-test-server/src/main/java/me/jaejoon/circuit_test_server/CircuitTestServer.java
@@ -2,8 +2,10 @@ package me.jaejoon.circuit_test_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class CircuitTestServer {
 
     public static void main(String[] args) {
--- circuit-test-server/src/main/java/me/jaejoon/circuit_test_server/service/TempServerCallService.java
+++ circuit-test-server/src/main/java/me/jaejoon/circuit_test_server/service/TempServerCallService.java
@@ -1,5 +1,6 @@
 package me.jaejoon.circuit_test_server.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,12 +11,18 @@ public class TempServerCallService {
 
     private final RestTemplate restTemplate;
 
+    @HystrixCommand(commandKey = "userName", fallbackMethod = "getCircuitUserName")
     public String userName() {
         return restTemplate.getForObject("/username", String.class);
     }
 
 
+    @HystrixCommand(commandKey = "userName", fallbackMethod = "getCircuitUserName")
     public String userNameSleep() {
         return restTemplate.getForObject("/username/timeout", String.class);
+    }
+
+    public String getCircuitUserName() {
+        return "fallback Jaejoon";
     }
 }
